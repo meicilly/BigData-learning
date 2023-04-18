71e0f7154cc41476791f2427068f2c24f28eaa35
meicilly <1240141465@qq.com>
2023-04-18 16:02:44 +0800

    redis-dine

--- redis/src/main/java/com/meicilly/connectionRedis.java
+++ redis/src/main/java/com/meicilly/connectionRedis.java
@@ -15,11 +15,11 @@ public class connectionRedis {
         //jedis = new Jedis("192,168.88.11",6379);
         // TODO: 2023-04-18 方法二 构建连接池对象
         JedisPoolConfig config = new JedisPoolConfig();
-        config.setMaxTotal(2);//总连接构建
-        config.setMaxIdle(2);//最大空闲连接
-        config.setMinIdle(1);//最小空闲连接
+        //config.setMaxTotal(2);//总连接构建
+        //config.setMaxIdle(2);//最大空闲连接
+        //config.setMinIdle(1);//最小空闲连接
         //构建连接池对象
-        JedisPool jedisPool = new JedisPool(config,"192,168.88.11",6379);
+        JedisPool jedisPool = new JedisPool(config,"192.168.88.11",6379);
         //获取连接
         jedis = jedisPool.getResource();
         jedis.set("s3","hadoop");
