8eba134c3e97c4f2cf7fa669ec426c158c997e8d
meicilly <1240141465@qq.com>
2023-06-08 16:53:24 +0800

    shell

--- shell/kafka/cluster_install_kafka.sh
+++ shell/kafka/cluster_install_kafka.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #规划集群的ip地址
-IP_ADDR="192.168.233.16 192.168.233.17 192.168.233.18"
+IP_ADDR="192.168.88.11 192.168.88.12 192.168.88.13"
 #当前目录 必须有这个目录
 CUR_PATH=/opt/meicilly
 #kafka的安装的路径
@@ -8,7 +8,8 @@ KAFKA_PATH=${CUR_PATH}/software
 #KAFKA的版本
 KAFKA_VER=kafka_2.12-3.0.0
 KAFKA_PAK=${KAFKA_VER}.tgz
-function install_component() {
+BROKER_ID=0
+function install_component(){
     echo "login in $1 host"
     ssh -o StrictHostKeyChecking=no root@$@
 }
@@ -17,6 +18,7 @@ function scp_package_and_file() {
 }
 for host in ${IP_ADDR}
 do
+  BROKER_ID=`expr ${BROKER_ID} + 1`
   #把初始化文件拷贝到 各个节点
   scp_package_and_file ${CUR_PATH}/kafka_init.sh $host ${CUR_PATH}
   #执行脚本
@@ -26,6 +28,6 @@ do
   #分发kafka_install.sh 文件到各个节点
   scp_package_and_file ${CUR_PATH}/kafka_install.sh $host ${CUR_PATH}
   #执行kafka_install.sh脚本
-  install_component $host "sh ${CUR_PATH}/kafka_install.sh ${KAFKA_PAK} ${KAFKA_PATH}"
+  install_component $host "sh ${CUR_PATH}/kafka_install.sh ${KAFKA_PAK} ${KAFKA_PATH} ${KAFKA_VER} ${BROKER_ID}"
 done
 
--- shell/kafka/kafka_install.sh
+++ shell/kafka/kafka_install.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
 KAFKA_PAK=$1
-KAFKA_PATH=$2
-echo $1
-echo $2
+CUR_PATH=$2
+KAFKA_PATH=$3
+BROKER_ID=$4
+function sed_param(){
+    mv ${CUR_PATH}/${KAFKA_PATH}/config/server.properties ${CUR_PATH}/${KAFKA_PATH}/config/server.properties.bak
+    sed "s/${1}/${2}/g" ${CUR_PATH}/${KAFKA_PATH}/config/server.properties.bak > ${CUR_PATH}/${KAFKA_PATH}/config/server.properties
+}
+echo $4
 #解压kafka
+cd ${CUR_PATH}
+tar -zxvf ${KAFKA_PAK} > kafka.log
+#进入kafka的目录 创建数据存储目录
 cd ${KAFKA_PATH}
-tar -zxvf ${KAFKA_PAK}
+if [ ! -d  data ];then
+  mkdir data
+fi
+#cat ${CUR_PATH}/${KAFKA_PATH}/config/server.properties
+#修改broker.id
+EDIT_BROKER_ID=broker.id=0
+EDITED_BROKER_ID=broker.id=${BROKER_ID}
+sed_param  ${EDIT_BROKER_ID} ${EDITED_BROKER_ID}
+cat ${CUR_PATH}/${KAFKA_PATH}/config/server.properties
+#修改数据目录
+REP_PATH=${CUR_PATH//\//\\/}
+EDIT_LOG_DIR="\/tmp\/kafka-logs"
+EDITED_LOG_DIR=${REP_PATH}\\/${KAFKA_PATH}\\/data
+#echo ${EDITED_LOG_DIR}
+#echo ${EDIT_LOG_DIR}
+sed_param ${EDIT_LOG_DIR} ${EDITED_LOG_DIR}
+
+
